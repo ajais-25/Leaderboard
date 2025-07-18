cd5a0f08303aa7f19a68395f03d11628b4602f1f
Akshat <akshatjaiswal2518@gmail.com>
2025-07-18 20:26:30 +0530

    client setup

---
+++ client/src/App.jsx
@@ -0,0 +1,10 @@
+function App() {
+  return (
+    <>
+      <h1 className="text-4xl text-center font-bold">Leaderboard</h1>
+      <div className="text-lg bg-green-300">This is Leaderboard</div>
+    </>
+  );
+}
+
+export default App;
